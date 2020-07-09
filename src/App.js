d137ab17e0ad757d5fa75c9fbd88b4863fb35979
Andriy <refwol131@mail.ru>
2020-07-10 00:43:42 +0300

    create Container component for MyPosts and Dialogs 43 done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 // import News from "./components/News/News";
 // import Music from "./components/Music/Music";
 // import Settings from "./components/Settings/Settings";
@@ -18,14 +18,13 @@ const App = (props) => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs' render={() =>
-                        <Dialogs
-                        store={props.store}
+                        <DialogsContainer
+                            store={props.store}
                         />}
                     />
                     <Route path='/profile' render={() =>
                         <Profile
-                            profilePage={props.state.profilePage}
-                            dispatch={props.dispatch}
+                            store={props.store}
                         />}
                     />
                     {/*<Route path='/news' component={News}/>*/}
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import style from './MyPosts.module.css'
 import Post from "./Post/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../Redux/profile-reducer";
 
 
 const MyPosts = (props) => {
@@ -10,15 +9,13 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef()
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator())
+    let onAddPost = () => {
+        props.addPost()
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        // props.updateNewPostText(text)
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action)
+        props.updateNewPostText(text)
     }
 
     return (
@@ -31,7 +28,7 @@ const MyPosts = (props) => {
                               onChange={onPostChange}
                     />
                 </div>
-                <button onClick={addPost}>Add post
+                <button onClick={onAddPost}>Add post
                 </button>
             </div>
             <div className={style.posts}>
