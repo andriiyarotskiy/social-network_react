83eab3bbf6907ef7b7777a5c82fb7c1207a9d2d5
Andriy <refwol131@mail.ru>
2020-07-25 00:36:29 +0300

    axios/Class Component/API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,67 +1,48 @@
 import React from "react";
 import styles from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png"
 
-const Users = (props) => {
-
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://cdn2.iconfinder.com/data/icons/people-occupation-job/64/Ninja-Warrior-Assassin-Japan-Fighter-Avatar-Martial_arts-512.png',
-                followed: true,
-                fullName: 'Andiy',
-                status: 'Junior',
-                location: {country: 'Ukraine', city: 'Kiev'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://cdn2.iconfinder.com/data/icons/people-occupation-job/64/Ninja-Warrior-Assassin-Japan-Fighter-Avatar-Martial_arts-512.png',
-                followed: false,
-                fullName: 'Voicek',
-                status: 'Middle',
-                location: {country: 'Poland', city: 'Krakow'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://cdn2.iconfinder.com/data/icons/people-occupation-job/64/Ninja-Warrior-Assassin-Japan-Fighter-Avatar-Martial_arts-512.png',
-                followed: false,
-                fullName: 'David',
-                status: 'Senior',
-                location: {country: 'England', city: 'London'}
-            },
-        ])
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
     }
-
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
                 </span>
-                <span>
                     <span>
-                        <div>{u.fullName}</div>
+                    <span>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 export default Users
